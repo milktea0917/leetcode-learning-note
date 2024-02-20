b6602ef788bec445e978b5acd631def2e9682760
milktea0917 <61684110+milktea0917@users.noreply.github.com>
2024-02-20 12:18:57 +0800

    Update TwoSum.py
    Add a new version to solve TwoSum.
    A dict() to help solve the problem in one pass.

--- EASY/TwoSum.py
+++ EASY/TwoSum.py
@@ -1,13 +1,27 @@
-# if Brute Force -> O(n^2)
+# Version2: Dict -> 
+
+# A dict() to help solve the problem in one pass
+class Solution(object):
+    def twoSum(self, nums, target):
+        dict = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] not in dict:
+                dict[nums[i]]=i
+            else:
+                return[dict[target-nums[i]], i]
+            
+####
+# Version1: Brute Force -> O(n^2)
 
 # final_return_list: list we return in final
-# difference: Deference of number two
+# difference: deference of two number 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         final_return_list = []
+        
         for index_num, content in enumerate(nums):
             difference = target - content
-          
             if difference in nums[index_num+1:]:
                     difference_index = nums[index_num+1:].index(difference)
                     final_return_list.append(index_num)
