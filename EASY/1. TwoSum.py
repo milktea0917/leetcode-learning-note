f9824d7dc6fae3f6d417a2d549c01a258739460c
milktea0917 <61684110+milktea0917@users.noreply.github.com>
2025-01-06 20:06:50 +0800

    Update 1. TwoSum.py

--- EASY/1. TwoSum.py
+++ EASY/1. TwoSum.py
@@ -1,10 +1,23 @@
+# Version3: 2025.01.06
+# if we use dictionary wisely, we can avoid using two for loop function
+# in dictionary, it doesn't need to be key = index all the time
+class Solution(object):
+    def twoSum(self, nums, target):
+        dict = {}
+        for i, content in enumerate(nums):
+            
+            if target-content in dict:
+                return [dict[target-content], i]
+            else:
+                dict[content] = i
+            
+
 # Version2: Dict -> O(n)
 
 # A dict() to help solve the problem in one pass
 class Solution(object):
     def twoSum(self, nums, target):
         dict = {}
-        
         for i in range(len(nums)):
             if target - nums[i] not in dict:
                 dict[nums[i]]=i
@@ -19,7 +32,6 @@ class Solution(object):
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         final_return_list = []
-        
         for index_num, content in enumerate(nums):
             difference = target - content
             if difference in nums[index_num+1:]:
